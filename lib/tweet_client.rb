704ea88bdcb86690d0318b4362b57d195d390764
kekoyana <kekoyana0@gmail.com>
2024-02-08 20:52:30 +0900

    パスワード未設定時のエラー追加

--- lib/tweet_client.rb
+++ lib/tweet_client.rb
@@ -6,6 +6,8 @@ class TweetClient
   PDS_URL = 'https://bsky.social'
 
   def post(message)
+    raise 'no setting password.' if PASSWORD.to_s.empty?
+
     credentials = Bskyrb::Credentials.new(USERNAME, PASSWORD)
     session = Bskyrb::Session.new(credentials, PDS_URL)
     bsky = Bskyrb::RecordManager.new(session)
