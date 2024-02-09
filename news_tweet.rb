87e2c0725a5bbb431ed0a11276fbacb752700daf
kekoyana <kekoyana0@gmail.com>
2024-02-09 15:36:23 +0900

    細かいところ微修正

--- news_tweet.rb
+++ news_tweet.rb
@@ -3,15 +3,16 @@
 require_relative 'lib/article_fetcher'
 require_relative 'lib/tweet_client'
 
-DEBUG_MODE = true
+DEBUG_MODE = false
 LAST_ID_FILE = File.expand_path('news_last_id', __dir__)
 NEWS_TEMPLATE = <<~TEMPLATE
-  QMAニュース
+  【QMAニュース】
   %<title>s
+
   %<url>s
 TEMPLATE
 
-recent_id = File.read(LAST_ID_FILE).to_i
+recent_id = File.exist?(LAST_ID_FILE) ? File.read(LAST_ID_FILE).to_i : 0
 fetcher = ArticleFetcher.new
 
 articles = fetcher.articles_from_last_id(recent_id)
@@ -19,8 +20,9 @@ articles = fetcher.articles_from_last_id(recent_id)
 # DEBUG用
 # articles.each { puts _1 }
 
-articles.each do |article|
+articles.reverse.each do |article|
   message = NEWS_TEMPLATE % { title: article.title, url: article.url }
+  message.strip!
   TweetClient.new.post(message)
 end
 
